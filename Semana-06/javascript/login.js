5564ff507ee3e4e527b876969b8ef8a08bc450e8
Guido Gentile <guido.gentile.1995@gmail.com>
2022-09-25 14:55:15 -0300

    BaSP-W06-2022: Login validations finished (probably I will modify email validation later)

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -1,13 +1,23 @@
 window.onload = function () {
+
+    var emailAlert = document.createElement("P");
+    var passwordAlert = document.createElement("P");
     var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
 
     email.onblur = function () {
         var email = document.getElementById("email");
         emailLength = email.value.length;
-        if (emailExpression.test(email.value)) {
+        if(emailLength == 0) {
+            emailAlert.innerHTML = "Email is required.";
+            email.insertAdjacentElement('afterend', emailAlert);
+        } else if (emailExpression.test(email.value)) {
             email.classList.add("green-border");
+            emailAlert.innerHTML = "";
+            email.insertAdjacentElement('afterend', emailAlert);
         } else {
             email.classList.add("red-border");
+            emailAlert.innerHTML = "Invalid email.";
+            email.insertAdjacentElement('afterend', emailAlert);
         }
     }
 
@@ -25,23 +35,26 @@ window.onload = function () {
         var passwordLength = password.value.length;
         if (passwordLength < 8) {
             password.classList.add("red-border");
+            passwordAlert.innerHTML = "Password must have at least 8 characters.";
+            password.insertAdjacentElement('afterend', passwordAlert);
         } else {
             for (i = 0; i < passwordLength; i++) {
-                if (password.value.charAt(i) == password.value.charAt(i).toUpperCase() && charUpper == 0) {
+                if (password.value.charAt(i) == password.value.charAt(i).toUpperCase() && isNaN(password.value.charAt(i)) && charUpper == 0) {
                     charUpper++;
-                } else if (password.value.charAt(i) == password.value.charAt(i).toLowerCase() && charLower == 0) {
+                } else if (password.value.charAt(i) == password.value.charAt(i).toLowerCase() && isNaN(password.value.charAt(i)) && charLower == 0) {
                     charLower++;
-                } else if (typeof Number(password.value.charAt(i)) == "number" && charNumber == 0) {
+                } else if (!isNaN(password.value.charAt(i)) && charNumber == 0) {
                     charNumber++;
                 }
                 if(charLower != 0 && charNumber != 0 && charUpper != 0) {
                     password.classList.add("green-border");
                     i = passwordLength;
-                    charUpper = 0;
-                    charLower = 0;
-                    charNumber = 0;
+                    passwordAlert.innerHTML = "";
+                    password.insertAdjacentElement('afterend', passwordAlert);
                 } else {
                     password.classList.add("red-border");
+                    passwordAlert.innerHTML = "Password must contain at least one uppercase, one lowercase and one number.";
+                    password.insertAdjacentElement('afterend', passwordAlert);
                 }
             }
         }
@@ -49,5 +62,8 @@ window.onload = function () {
 
     password.onfocus = function () {
         password.classList.remove("green-border", "red-border");
+        charUpper = 0;
+        charLower = 0;
+        charNumber = 0;
     }
 }
